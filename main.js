efc2de42924f15e9ccd266c49891f786f890a086
unknown <hello@h.com>
2019-06-29 16:56:53 +0600

    as

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ window.onload = () => {
         { name: 'Online Learning', link: '/project_1/', img: 'project1.jpg' },
         { name: 'Portfolio', link: '/project_2/', img: 'portfolio.jpg' },
         { name: 'Todo Vue', link: '/project_3/', img: 'project3.jpg' },
-        { name: 'Not Done Yet', link: '#', img: 'working.jpg' },
+        { name: 'E-Commerce', link: '/project_4/', img: 'project4.jpg' },
         { name: 'Not Done Yet', link: '#', img: 'working.jpg' }
       ]
     }
